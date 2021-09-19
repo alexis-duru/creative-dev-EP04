94d424c9cb01638b0a1b6ed0103672b7918d04f6
alexis duru <alexis.duru.pro@gmail.com>
2021-09-19 19:40:52 +0200

    v1

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: 'assets/img/img1.jpg',
+    image2: 'assets/img/img2.jpg',
+    angle: Math.PI / 8,
+    displacementImage: 'assets/img/diss.png',
+});
+
